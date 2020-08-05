753af6ce4d72b4fa6f47706ce813ce7674640e2d
ImageOfDarkness <sumbaev.leha@mail.ru>
2020-08-05 16:16:38 +0400

    last commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,27 +1,57 @@
 import React, { Component, useState } from "react";
 import Message from "./Message";
 
-const Chat = () => {
-  return (
-    <div className="chat">
-      <div className="conteiner chat__container">
-        <div className="bring-out">
-          <Message text="доме, офисе, кабинете, мастерской" self />
-          <Message text="доме, офисе, кабинете, мастерской" self />
-          <Message text="Это означает," />
-          <div className="chat__date-separator">
-            {new Date().toLocaleTimeString()}
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "text",
+      messeges: [
+        { text: "text", self: true },
+        { text: "text2", self: false },
+      ],
+    };
+  }
+  render() {
+    const { messeges, text } = this.state;
+    return (
+      <div className="chat">
+        <div className="conteiner chat__container">
+          <div className="bring-out">
+            <div className="chat__date-separator">
+              {new Date().toLocaleTimeString()}
+            </div>
+            {messeges.map((item) => (
+              <Message self={item.self} text={item.text} />
+            ))}
           </div>
-          <Message text="доме, офисе, кабинете, мастерской" self />
-          <Message text="Это означает," />
-        </div>
-        <div className="chat__send-text">
-          <textarea placeholder="Введите текст" className="poleText"></textarea>
-          <button className="buttonAdd">Отправить</button>
+          <div className="chat__send-text">
+            <textarea
+              onChange={(e) => {
+                this.setState({ text: e.target.value });
+              }}
+              value={text}
+              placeholder="Введите текст"
+              className="poleText"
+            ></textarea>
+            <button
+              onClick={() => {
+                let newMesseges = messeges;
+                newMesseges.push({ text: text, self: true });
+                this.setState({
+                  text: "",
+                  messeges: newMesseges,
+                });
+              }}
+              className="buttonAdd"
+            >
+              Отправить
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Chat;
